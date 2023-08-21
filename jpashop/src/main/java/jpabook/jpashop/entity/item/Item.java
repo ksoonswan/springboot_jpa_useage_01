00b4b0a6840eb669a0857dd7969001703242c1d7
soonswan <swan.kim@oysterable.com>
2023-08-21 20:12:42 +0900

    [add] 객체지향적으로 상품 재고관리 하기

--- jpashop/src/main/java/jpabook/jpashop/entity/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/entity/item/Item.java
@@ -11,6 +11,7 @@ import jakarta.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
 import jpabook.jpashop.entity.OrderItem;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -32,5 +33,25 @@ public abstract class Item {
 
   @OneToMany(mappedBy = "item")
   private List<OrderItem> orderItems = new ArrayList<>();
+
+  //==비즈니스 로직==//
+
+  /**
+   * stock 증가
+   */
+  public void addStock(int quantity) {
+    this.stockQuantity += quantity;
+  }
+
+  /**
+   * stock 감소
+   */
+  public void removeStock(int quantity) {
+    int restStock = this.stockQuantity - quantity;
+    if (restStock < 0) {
+      throw new NotEnoughStockException("need more stock");
+    }
+    this.stockQuantity = restStock;
+  }
 
 }
